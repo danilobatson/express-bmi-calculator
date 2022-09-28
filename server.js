b6ca1ffb2dede46b9b1f9fb1cb80861bf1d6b127
danilobatson <djbatson19@gmail.com>
2022-09-27 17:59:15 -0700

    Converted to BMI Calculator

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/bmi.html'));
 
 app.post('/', (req, res) => {
   const { weight, height } = req.body;
@@ -14,8 +14,5 @@ app.post('/', (req, res) => {
   res.send(`Your BMI is: ${Math.trunc(result)}`);
 }); 
 
-app.get('/about', (req, res) =>
-  res.send('<p>My name is Danilo and I am a developer</p>')
-);
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
