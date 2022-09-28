1b35fb1f9c13d905d330fd8efb0a883d15c3d065
danilobatson <djbatson19@gmail.com>
2022-09-27 17:16:02 -0700

    About route setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.get('/about', (req, res) => res.send('<p>My name is Danilo and I am a developer</p>'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server started on port ${port}!`));
