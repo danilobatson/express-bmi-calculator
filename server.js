c6ebeba05b3dcbcd2e28a2ee8542ea28e61f2c55
danilobatson <djbatson19@gmail.com>
2022-09-27 17:26:05 -0700

    Index.html skeleton

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
-app.get('/about', (req, res) => res.send('<p>My name is Danilo and I am a developer</p>'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/about', (req, res) =>
+  res.send('<p>My name is Danilo and I am a developer</p>')
+);
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
