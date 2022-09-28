e17fd8b30c12d8cb8aa2fbb38367dc4c31555a13
danilobatson <djbatson19@gmail.com>
2022-09-27 17:44:45 -0700

    Post setup

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.post('/', (req, res) => {
+  const { n1, n2 } = req.body;
+  const result = Number(n1) + Number(n2);
+  res.send(`The result of the calculation is: ${result}`);
+});
+
 app.get('/about', (req, res) =>
   res.send('<p>My name is Danilo and I am a developer</p>')
 );
